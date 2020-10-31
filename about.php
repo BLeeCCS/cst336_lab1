1ffb9afe156a8e0c2b1db4d06ec468ad48312507
EC2 Default User <ec2-user@ip-172-31-12-47.us-west-1.compute.internal>
2020-10-31 23:57:35 +0000

    Lesson 6

--- about.php
+++ about.php
@@ -13,9 +13,9 @@
     </header>
     <hr />
     <nav>
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a href="contact.html">Contact</a>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
         
         <br /><br />
         
