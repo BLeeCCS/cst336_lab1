2decee8c4071c0cac101775703d08811593816b8
EC2 Default User <ec2-user@ip-172-31-12-47.us-west-1.compute.internal>
2020-10-31 21:25:35 +0000

    Lesson 3

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Brandon Lee: Personal Website</title>
+</head>
+<body>
+    <header>
+        <h1> Brandon Lee </h1>
+    </header>
+    <hr />
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+        
+        <br /><br />
+        
+        <main>
+            
+            <figure>
+                <img src="img/Brandon_Lee.PNG" alt="Picture of Brandon Lee"></img>
+            </figure>
+            
+            <div>
+                Hello! <br />
+                <p>Thank you for visiting my professional portfolio website.</p>
+                <p>I am a CSUMB student and major in Computer Science, focusing in Software Engineering.</p>
+                <p>Feel free to contact me!</p>
+                
+                <br /><br />
+                
+                <em>"The <strong>secret</strong> of getting ahead is getting started"</em><br />
+                - Mark Twain
+            </div>
+        </main>
+    </nav>
+</body>
+</html>
