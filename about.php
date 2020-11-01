4536788a825767b5fa74ed8d859da48918d076b3
EC2 Default User <ec2-user@ip-172-31-12-47.us-west-1.compute.internal>
2020-11-01 00:05:27 +0000

    lesson 7

--- about.php
+++ about.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Brandon Lee: Personal Website</title>
     <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Brandon Lee: Personal Website</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1> Brandon Lee </h1>
+    </header>
+    <hr />
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
+        
+        <br /><br />
+        
+        <main>
+          <div id="content">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfH0EIwMtFkLoANBKI2iBLoTkH186qij0O5yJ1-s8P7522_A/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+          </div>
+        </main>
+    </nav>
+    <footer>
+        <hr>
+        CST336 Internet Programming. 2020&copy; Lee <br />
+        <strong>Disclaimer:</strong> The information in this webpage is gictitious. <br />
+        It is used for academic purposes only.
+        <figure>
+            <img src="img/CSUMB_logo.png"></img>
+        </figure>
+    </footer>
+</body>
+</html>
